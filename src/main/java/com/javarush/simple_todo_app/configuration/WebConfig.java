bdc1a7d9ae078cb0a091fd55b493bde7eb91a854
Aleksandr Bogomolov <ksandr.ua@gmail.com>
2016-04-02 11:16:12 +0300

    Add style.css, finish complete

--- src/main/java/com/javarush/simple_todo_app/configuration/WebConfig.java
+++ src/main/java/com/javarush/simple_todo_app/configuration/WebConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -29,10 +28,5 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/css/**").addResourceLocations("/css/");
-    }
-
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
     }
 }
--- src/main/java/com/javarush/simple_todo_app/dao/toDoDaoImpl.java
+++ src/main/java/com/javarush/simple_todo_app/dao/toDoDaoImpl.java
@@ -7,6 +7,7 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.Iterator;
 import java.util.List;
 
 @Repository("toDoDao")
@@ -15,7 +16,7 @@ public class ToDoDaoImpl implements ToDoDao {
     @Autowired
     private SessionFactory sessionFactory;
 
-    protected Session getSession() {
+    private Session getSession() {
         return sessionFactory.getCurrentSession();
     }
 
@@ -34,5 +35,24 @@ public class ToDoDaoImpl implements ToDoDao {
     public List<ToDo> findAllToDo() {
         Criteria criteria = getSession().createCriteria(ToDo.class);
         return (List<ToDo>) criteria.list();
+    }
+
+    public List<ToDo> showDoneToDo() {
+        return showToDo(true);
+    }
+
+    public List<ToDo> showNotDoneToDo() {
+        return showToDo(false);
+    }
+
+    private List<ToDo> showToDo(Boolean done) {
+
+        List<ToDo> list = findAllToDo();
+        Iterator iterator = list.iterator();
+        while (iterator.hasNext()) {
+            ToDo toDo = (ToDo) iterator.next();
+            if (toDo.getDone() != done) iterator.remove();
+        }
+        return list;
     }
 }
